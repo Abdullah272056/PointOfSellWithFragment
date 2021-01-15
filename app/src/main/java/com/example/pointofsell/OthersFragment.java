0492794811f71d2a98815b3daab4efa49349edf4
Abdullah272056 <abdullah272056@gmail.com>
2021-01-15 21:03:56 +0600

    view finding

--- app/src/main/java/com/example/pointofsell/OthersFragment.java
+++ app/src/main/java/com/example/pointofsell/OthersFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,21 +12,47 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.pointofsell.retrofit.ApiInterface;
+
 public class OthersFragment extends Fragment {
+    TextView customerTextView,productTextView,invoiceTextView;
+    TextView totalSaleAmountTextView,totalSoldProductQuantityTextView,totalSoldInvoiceTextView,
+            totalDueAmountTextView,totalProfitTextView;
+    TextView totalProductCostTextView,totalProductStockTextView,totalProductTypeTextView,extraInfoTextView;
+    TextView customerCountTextView;
+    Button sellInfoByDateButton;
+
+    ApiInterface apiInterface;
+    String token;
+    String customerCount;
+    View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View viewP=inflater.inflate(R.layout.others_fragment,container,false);
+         view=inflater.inflate(R.layout.others_fragment,container,false);
+            Bundle bundle=this.getArguments();
+            token=bundle.getString("token");
 
-        Bundle bundle=this.getArguments();
-        String token=bundle.getString("token");
-        TextView txt=viewP.findViewById(R.id.txt);
-        txt.setText(token);
+        //finding textView
+        customerCountTextView=view.findViewById(R.id.customerCountId);
+        totalSaleAmountTextView=view.findViewById(R.id.totalSaleAmountTextViewId);
+        totalSoldProductQuantityTextView=view.findViewById(R.id.totalSoldProductQuantityTextViewId);
+        totalSoldInvoiceTextView=view.findViewById(R.id.totalSoldInvoiceTextViewId);
+        totalDueAmountTextView=view.findViewById(R.id.totalDueAmountTextViewId);
+        totalProfitTextView=view.findViewById(R.id.totalProfitTextViewId);
+        totalProductCostTextView=view.findViewById(R.id.totalProductCostTextViewId);
+        totalProductStockTextView=view.findViewById(R.id.totalProductStockTextViewId);
+        totalProductTypeTextView=view.findViewById(R.id.totalProductTypeTextViewId);
+
+
+
+//        TextView txt=view.findViewById(R.id.txt);
+//        txt.setText(token);
       //  Toast.makeText(HomePage.this, "", Toast.LENGTH_SHORT).show();
 
 
 
-        return viewP;
+        return view;
     }
 
 
