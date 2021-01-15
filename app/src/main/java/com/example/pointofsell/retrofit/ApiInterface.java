86e765b504202b23dfca46d4ef25ce9c14182f59
Abdullah272056 <abdullah272056@gmail.com>
2021-01-15 15:21:46 +0600

    create register model class and create interface

--- app/src/main/java/com/example/pointofsell/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/pointofsell/retrofit/ApiInterface.java
@@ -1,4 +1,14 @@
 package com.example.pointofsell.retrofit;
-public interface ApiInterface {
 
+import com.example.pointofsell.register.RegistrationData;
+import com.example.pointofsell.register.RegistrationResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+    // registration
+    @POST("api/auth/register/")
+    Call<RegistrationResponse> registrationData(@Body RegistrationData registrationData);
 }
