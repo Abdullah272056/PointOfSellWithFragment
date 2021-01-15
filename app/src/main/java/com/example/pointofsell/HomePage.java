5caa59ad6359282319e4abc5171dd9e343efa9a1
Abdullah272056 <abdullah272056@gmail.com>
2021-01-15 20:59:52 +0600

    token pass and receive

--- app/src/main/java/com/example/pointofsell/HomePage.java
+++ app/src/main/java/com/example/pointofsell/HomePage.java
@@ -12,22 +12,34 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePage extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    Bundle bundle;
+    Fragment fragment=null;
+    String token;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         bottomNavigationView=findViewById(R.id.bottomBarId);
+        token=getIntent().getStringExtra("token");
+
+         bundle=new Bundle();
+        bundle.putString("token",token);
+
         if (savedInstanceState==null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.frameViewId,new OthersFragment()).commit();
+             fragment=new OthersFragment();
+            fragment.setArguments(bundle);
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameViewId,fragment).commit();
 
         }
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment=null;
+
 
                 if (item.getItemId()==R.id.homeItemId){
                     fragment=new OthersFragment();
+
                 }
                 if (item.getItemId()==R.id.customerItemId){
                     fragment=new OthersFragment();
@@ -42,6 +54,7 @@ public class HomePage extends AppCompatActivity {
                     fragment=new OthersFragment();
 //                    Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
                 }
+                fragment.setArguments(bundle);
                 getSupportFragmentManager().beginTransaction().replace(R.id.frameViewId,fragment).commit();
 
                 return true;
--- app/src/main/java/com/example/pointofsell/OthersFragment.java
+++ app/src/main/java/com/example/pointofsell/OthersFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,14 @@ public class OthersFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View viewP=inflater.inflate(R.layout.others_fragment,container,false);
+
+        Bundle bundle=this.getArguments();
+        String token=bundle.getString("token");
+        TextView txt=viewP.findViewById(R.id.txt);
+        txt.setText(token);
+      //  Toast.makeText(HomePage.this, "", Toast.LENGTH_SHORT).show();
+
+
 
         return viewP;
     }
