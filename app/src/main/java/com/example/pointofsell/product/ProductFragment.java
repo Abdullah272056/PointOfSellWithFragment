ccd49ba2b22e08a7101bca69b309c98563df1fc5
Abdullah272056 <abdullah272056@gmail.com>
2021-01-16 15:09:01 +0600

    create ProductFragment.java  and delete pro class

---
+++ app/src/main/java/com/example/pointofsell/product/ProductFragment.java
@@ -0,0 +1,25 @@
+package com.example.pointofsell.product;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.pointofsell.R;
+
+public class ProductFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.others_fragment, container, false);
+
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/pointofsell/product/pro.java
+++
@@ -1,4 +0,0 @@
-package com.example.pointofsell.product;
-
-public class pro {
-}
