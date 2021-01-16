e5307f77be46f24d72d256d29efe36d3b2e99a7f
Abdullah272056 <abdullah272056@gmail.com>
2021-01-16 22:11:36 +0600

    create Invoice_Fragment.java

---
+++ app/src/main/java/com/example/pointofsell/invoice/Invoice_Fragment.java
@@ -0,0 +1,7 @@
+package com.example.pointofsell.invoice;
+
+import androidx.fragment.app.Fragment;
+
+public class Invoice_Fragment extends Fragment {
+
+}
--- app/src/main/java/com/example/pointofsell/invoice/asd.java
+++
@@ -1,4 +0,0 @@
-package com.example.pointofsell.invoice;
-
-public class asd {
-}
