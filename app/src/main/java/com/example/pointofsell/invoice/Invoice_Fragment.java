0d73a6d2ed6c63fc4bce733cb6a8c03fbe306920
Abdullah272056 <abdullah272056@gmail.com>
2021-01-16 22:23:05 +0600

    view finding

--- app/src/main/java/com/example/pointofsell/invoice/Invoice_Fragment.java
+++ app/src/main/java/com/example/pointofsell/invoice/Invoice_Fragment.java
@@ -4,19 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pointofsell.R;
+import com.example.pointofsell.retrofit.ApiInterface;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class Invoice_Fragment extends Fragment {
+    ApiInterface apiInterface;
+    String token;
+//    List<Invoice> invoiceList;
+//    InvoiceCustomAdapter invoiceCustomAdapter;
+    RecyclerView invoiceRecyclerView;
+    ProgressBar invoiceProgressBar;
+    FloatingActionButton addInvoiceButton;
+
+
+
     View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.invoice_fragment, container, false);
+
+        invoiceRecyclerView=view.findViewById(R.id.invoiceRecyclerViewId);
+        invoiceProgressBar=view.findViewById(R.id.invoiceProgressBarId);
+        addInvoiceButton=view.findViewById(R.id.addInvoiceButtonId);
 
 
         return view;
