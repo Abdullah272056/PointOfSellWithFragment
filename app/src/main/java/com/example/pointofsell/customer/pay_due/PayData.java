9533e14527ff63d47e353ec1fc1dc3475bee8ac4
Abdullah272056 <abdullah272056@gmail.com>
2021-01-16 13:12:21 +0600

    create pay_due package and some class and add code

---
+++ app/src/main/java/com/example/pointofsell/customer/pay_due/PayData.java
@@ -0,0 +1,37 @@
+package com.example.pointofsell.customer.pay_due;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PayData {
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("payAmount")
+    @Expose
+    private Integer payAmount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getPayAmount() {
+        return payAmount;
+    }
+
+    public void setPayAmount(Integer payAmount) {
+        this.payAmount = payAmount;
+    }
+
+    public PayData(String id, Integer payAmount) {
+        this.id = id;
+        this.payAmount = payAmount;
+    }
+
+    public PayData() {
+    }
+}
