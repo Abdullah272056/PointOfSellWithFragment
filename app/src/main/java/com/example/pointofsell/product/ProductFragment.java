13bbd9d81ca7e7f29be90f6819a4d557c70024d5
Abdullah272056 <abdullah272056@gmail.com>
2021-01-16 15:23:37 +0600

    create getAllProduct method and getall product

--- app/src/main/java/com/example/pointofsell/product/ProductFragment.java
+++ app/src/main/java/com/example/pointofsell/product/ProductFragment.java
@@ -12,25 +12,35 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pointofsell.R;
+import com.example.pointofsell.product.delete_product.GetProductData;
+import com.example.pointofsell.product.get_product.GetProductDataResponse;
+
 import com.example.pointofsell.retrofit.ApiInterface;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProductFragment extends Fragment {
     String productName,productRegularPrice,productSellingPrice,productStock,productDescription,unitType;
 
     Spinner unitTypeSpinner;
-    //List<GetProductData> getProductDataList;
+    List<GetProductData> getProductDataList;
     String token;
     ApiInterface apiInterface;
     //ProductCustomAdapter productCustomAdapter;
@@ -66,7 +76,41 @@ public class ProductFragment extends Fragment {
         productProgressBar=view.findViewById(R.id.productProgressBarId);
         addProductButton=view.findViewById(R.id.addProductButtonId);
        // unitTypeSpinner=view.findViewById(R.id.unitTypeSpinnerId);
-
+        getAllProduct();
         return view;
     }
+
+
+    public void getAllProduct() {
+        apiInterface.getAllProduct("Bearer "+token).enqueue(new Callback<GetProductDataResponse>() {
+            @Override
+            public void onResponse(Call<GetProductDataResponse> call, Response<GetProductDataResponse> response) {
+
+                if (response.code()==200){
+                    getProductDataList=new ArrayList<>();
+                    getProductDataList.addAll(response.body().getProducts());
+                    // Toast.makeText(ProductActivity.this, String.valueOf(getProductDataList.size()), Toast.LENGTH_SHORT).show();
+
+//                    productCustomAdapter = new ProductCustomAdapter(getActivity(),token,getProductDataList);
+//                    productRecyclerView.setLayoutManager(new LinearLayoutManager(ProductActivity.this));
+//                    productRecyclerView.setAdapter(productCustomAdapter);
+
+                    Toast.makeText(getActivity(), "All product fetched", Toast.LENGTH_SHORT).show();
+                }else if (response.code()==404){
+                    Toast.makeText(getActivity(), "Product not found", Toast.LENGTH_SHORT).show();
+                }
+                else if (response.code()==401){
+                    Toast.makeText(getActivity(), "Invalid token", Toast.LENGTH_SHORT).show();
+                }else {
+                }
+
+                productProgressBar.setVisibility(View.GONE);
+            }
+            @Override
+            public void onFailure(Call<GetProductDataResponse> call, Throwable t) {
+                productProgressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
 }
