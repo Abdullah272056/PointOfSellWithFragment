b7d9ebe7a00f4a57a928a64d8a79afca11147158
Abdullah272056 <abdullah272056@gmail.com>
2021-01-18 00:33:12 +0600

    create SingleCustomerTotalSellFragment.java

---
+++ app/src/main/java/com/example/pointofsell/customer/SingleCustomerTotalSellFragment.java
@@ -0,0 +1,26 @@
+package com.example.pointofsell.customer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.pointofsell.R;
+
+public class SingleCustomerTotalSellFragment extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.single_customer_total_sell_fragment,container,false);
+
+
+
+    return view;
+    }
+}
