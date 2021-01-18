e82e0f83d7c69e7d9ee37fa88addfe1d145a768c
Abdullah272056 <abdullah272056@gmail.com>
2021-01-18 15:36:19 +0600

    extended fragment

--- app/src/main/java/com/example/pointofsell/customer/DuePayHistoryFragment.java
+++ app/src/main/java/com/example/pointofsell/customer/DuePayHistoryFragment.java
@@ -1,5 +1,25 @@
 package com.example.pointofsell.customer;
 
-public class DuePayHistoryFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.pointofsell.R;
+
+public class DuePayHistoryFragment extends Fragment {
+   View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.customer_fragment, container, false);
     
-}
+
+
+    return view;
+    }
+    }
