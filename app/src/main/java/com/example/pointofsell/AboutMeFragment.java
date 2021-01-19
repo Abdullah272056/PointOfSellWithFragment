8c83cf87baf7ea53cb9e2fc7e03681d26e05c588
Abdullah272056 <abdullah272056@gmail.com>
2021-01-19 14:22:42 +0600

    extended Fragment in AboutMeFragment.java

--- app/src/main/java/com/example/pointofsell/AboutMeFragment.java
+++ app/src/main/java/com/example/pointofsell/AboutMeFragment.java
@@ -1,4 +1,23 @@
 package com.example.pointofsell;
 
-public class AboutMeFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AboutMeFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.about_me_fragment,container,false);
+
+
+
+        return view;
+}
 }
