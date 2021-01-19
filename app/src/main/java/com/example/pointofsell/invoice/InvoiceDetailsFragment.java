47a05680e536f53eea3c888b8eafc7f3713ccec5
Abdullah272056 <abdullah272056@gmail.com>
2021-01-19 21:31:39 +0600

    extended fragment

--- app/src/main/java/com/example/pointofsell/invoice/InvoiceDetailsFragment.java
+++ app/src/main/java/com/example/pointofsell/invoice/InvoiceDetailsFragment.java
@@ -1,5 +1,26 @@
 package com.example.pointofsell.invoice;
 
-public class InvoiceDetailsFragment {
-    
-}
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.pointofsell.R;
+
+public class InvoiceDetailsFragment extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.invoice_details_fragment, container, false);
+
+
+
+        return view;
+    }
+    }
