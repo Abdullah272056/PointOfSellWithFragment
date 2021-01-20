b7715ab3fb69bc0540a11139112d73519e41419d
Abdullah272056 <abdullah272056@gmail.com>
2021-01-20 18:33:09 +0600

    extended fragment

--- app/src/main/java/com/example/pointofsell/SellInfoByDayFragment.java
+++ app/src/main/java/com/example/pointofsell/SellInfoByDayFragment.java
@@ -1,6 +1,24 @@
 package com.example.pointofsell;
 
-public class SellInfoByDayFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-    
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SellInfoByDayFragment extends Fragment {
+
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.invoice_fragment, container, false);
+
+
+
+        return view;
+    }
 }
